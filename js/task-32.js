6c07a28ad4e741f653a6084e87c78a2c8f1da2d1
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-01-08 04:31:21 +0200

    Update task-32.js

--- js/task-32.js
+++ js/task-32.js
@@ -1,19 +1,19 @@
-// function includes(array, value) {
-//     // Пиши код ниже этой строки
-//     for (let i = 0; i < array.length; i += 1) {
-//         if (array[i] === value) {
-//         return true;
-//       }
-//     }
+function includes(array, value) {
+    // Пиши код ниже этой строки
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i] === value) {
+        return true;
+      }
+    }
     
-//     return false;
-//     // Пиши код выше этой строки
-//   }
+    return false;
+    // Пиши код выше этой строки
+  }
 
   
-//   console.log(includes([1, 2, 3, 4, 5], 3));
-//   console.log(includes([1, 2, 3, 4, 5], 17));
-//   console.log(includes(['Земля', 'Марс', 'Венера', 'Юпитер', 'Сатурн'], 'Юпитер'));
-//   console.log(includes(['Земля', 'Марс', 'Венера', 'Юпитер', 'Сатурн'], 'Уран'));
-//   console.log(includes(['яблоко', 'слива', 'груша', 'апельсин'], 'слива'));
-//   console.log(includes(['яблоко', 'слива', 'груша', 'апельсин'], 'киви'));
+  console.log(includes([1, 2, 3, 4, 5], 3));
+  console.log(includes([1, 2, 3, 4, 5], 17));
+  console.log(includes(['Земля', 'Марс', 'Венера', 'Юпитер', 'Сатурн'], 'Юпитер'));
+  console.log(includes(['Земля', 'Марс', 'Венера', 'Юпитер', 'Сатурн'], 'Уран'));
+  console.log(includes(['яблоко', 'слива', 'груша', 'апельсин'], 'слива'));
+  console.log(includes(['яблоко', 'слива', 'груша', 'апельсин'], 'киви'));
